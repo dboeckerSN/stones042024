2696450348c6ad0c1de0c851a093c22e4b0c641a
Dominik Böcker <dominik.boecker@sn-invent.de>
2024-04-08 14:25:06 +0200

    Aufgabe 2.1

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
+import { ProductComponent } from './product/product.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent
+  ],
   imports: [
     CommonModule,
     ProductRoutingModule
+  ],
+  exports: [
+    ProductComponent
   ]
 })
 export class ProductModule { }
---
+++ src/app/product/product/product.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductComponent } from './product.component';
+
+describe('ProductComponent', () => {
+  let component: ProductComponent;
+  let fixture: ComponentFixture<ProductComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ProductComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ProductComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('raisePrice should raise price by 5', () => {
+    const previousPrice = component.product.price;
+
+    component.raisePrice();
+
+    expect(component.product.price).toBe(previousPrice + 5);
+  });
+});
