35d221bf3b6dac5e435d157eb2e478195e20cfa1
Dominik Böcker <dominik.boecker@sn-invent.de>
2024-04-09 10:54:29 +0200

    Aufgabe 3

--- src/app/product/product/product.component.ts
+++ src/app/product/product/product.component.ts
@@ -10,6 +10,19 @@ export class ProductComponent {
   @Input({ required: true }) product!: Product;
   @Output() priceChange = new EventEmitter<number>();
 
+  showPrice = true;
+  styleConfig: any = {
+    borderStyle: 'dashed',
+  };
+
+  togglePrice() {
+    this.showPrice = !this.showPrice;
+  }
+
+  changePrice(price: number) {
+    this.product.price = price;
+  }
+
   raisePrice() {
     this.product.price += 5;
     this.priceChange.emit(this.product.price);
